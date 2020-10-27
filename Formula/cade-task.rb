dce55921fe8bd92d1566c65b1db0b38228f02cd5
Cade Ekblad-Frank <cade@e-f.me>
2020-10-27 00:07:33 -0500

    Use package resoure for click since brew doesn't support .whl; update to new formats not specified in docs

--- Formula/cade-task.rb
+++ Formula/cade-task.rb
@@ -3,24 +3,29 @@ class CadeTask < Formula
 
   desc "Dirty wrapper around reminders-cli"
   homepage "https://github.com/cadeef/cade-task"
+  # TODO: Add stable source package
+  head "https://github.com/cadeef/cade-task.git", branch: "main"
 
-  head "https://github.com/cadeef/cade-task.git", :branch => "main"
-
-  depends_on :python3
+  depends_on "python@3.9"
 
   resource "click" do
-    url "https://files.pythonhosted.org/packages/d2/3d/fa76db83bf75c4f8d338c2fd15c8d33fdd7ad23a9b5e57eb6c5de26b430e/click-7.1.2-py2.py3-none-any.whl#sha256=dacca89f4bfadd5de3d7489b7c8a566eee0d3676333fbb50030263894c38c0dc"
-    sha256 "dacca89f4bfadd5de3d7489b7c8a566eee0d3676333fbb50030263894c38c0dc"
+    url "https://files.pythonhosted.org/packages/27/6f/be940c8b1f1d69daceeb0032fee6c34d7bd70e3e649ccac0951500b4720e/click-7.1.2.tar.gz#sha256=d2b5255c7c6349bc1bd1e59e08cd12acbbd63ce649f2588755783aa94dfb6b1a"
+    sha256 "d2b5255c7c6349bc1bd1e59e08cd12acbbd63ce649f2588755783aa94dfb6b1a"
   end
 
   resource "tablib" do
     url "https://files.pythonhosted.org/packages/cf/1a/6c033d742f544c192b55093ae927d9ad52d7e290ac06ac9fe6f94239d5f9/tablib-2.0.0.tar.gz#sha256=8cc2fa10bc37219ac5e76850eb7fbd50de313c7a1a7895c44af2a8dd206b7be7"
-    sha256 "8cc2fa10bc37219ac5e76850eb7fbd50de313c7a1a7895c44af2a8dd206b7be7"    
+    sha256 "8cc2fa10bc37219ac5e76850eb7fbd50de313c7a1a7895c44af2a8dd206b7be7"
+  end
+
+  resource "tabulate" do
+    url "https://files.pythonhosted.org/packages/57/6f/213d075ad03c84991d44e63b6516dd7d185091df5e1d02a660874f8f7e1e/tabulate-0.8.7.tar.gz#sha256=db2723a20d04bcda8522165c73eea7c300eda74e0ce852d9022e0159d7895007"
+    sha256 "db2723a20d04bcda8522165c73eea7c300eda74e0ce852d9022e0159d7895007"
   end
 
   def install
     virtualenv_install_with_resources
   end
 
-  # TODO: Add your package's tests here
+  # TODO: Add your package's tests
 end
