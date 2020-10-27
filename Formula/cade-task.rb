d03d2c1f233240582e4962d4d23e0148d0985462
Cade Ekblad-Frank <cade@e-f.me>
2020-10-27 00:34:43 -0500

    Brew test-bot doesn't like the license before the URL

--- Formula/cade-task.rb
+++ Formula/cade-task.rb
@@ -3,10 +3,10 @@ class CadeTask < Formula
 
   desc "Dirty wrapper around reminders-cli"
   homepage "https://github.com/cadeef/cade-task"
-  license "MIT"
   url "https://github.com/cadeef/cade-task/archive/v0.1.0.tar.gz"
   sha256 "e53f6935f0983d09aa38bd3e3cb4d6c8266dab5ac354fc0d723ba0aeffa55667"
   head "https://github.com/cadeef/cade-task.git", branch: "main"
+  license "MIT"
 
   depends_on "python@3.9"
 
