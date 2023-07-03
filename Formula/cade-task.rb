111d1a7b207bf606662d36c48bd3ed1f4b3da4c4
Cade Ekblad-Frank <cade@e-f.me>
2023-07-03 03:29:43 -0500

    Reorder depends

--- Formula/cade-task.rb
+++ Formula/cade-task.rb
@@ -8,8 +8,8 @@ class CadeTask < Formula
   license "MIT"
   head "https://github.com/cadeef/cade-task.git", branch: "main"
 
-  depends_on "python@3.11"
   depends_on "cmake" => :build
+  depends_on "python@3.11"
 
   resource "click" do
     url "https://files.pythonhosted.org/packages/59/87/84326af34517fca8c58418d148f2403df25303e02736832403587318e9e8/click-8.1.3.tar.gz"
